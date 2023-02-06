0a882cc50763edb27225c73d4998728f63800d3d
German Zlobin <germanzlobin2@gmail.com>
2023-02-06 16:27:19 +0700

    second

---
+++ main.py
@@ -0,0 +1,38 @@
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtGui import QPainter
+from random import randint
+from PyQt5 import uic
+
+
+class Example(QWidget):
+    def __init__(self):
+        super(Example, self).__init__()
+        uic.loadUi('circle.ui', self)
+        self.setWindowTitle('Circle')
+        self.pushButton.clicked.connect(self.click)
+        self.show()
+        self.check = False
+
+    def click(self):
+        self.check = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.check:
+            qp = QPainter()
+            qp.begin(self)
+            r = randint(10, 200)
+            x, y = self.size().width(), self.size().height()
+            qp.drawEllipse((x - r) // 2, (y - r) // 2, r, r)
+            qp.end()
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
